ea9a1b98aaac53de8e88b9066b3558b6b1e0fb14
Xiang Yang <Ryu-Yang@qq.com>
2025-09-24 20:30:53 +0800

    complete aloha docker

--- operating_platform/robot/robots/configs.py
+++ operating_platform/robot/robots/configs.py
@@ -187,8 +187,8 @@ class AlohaRobotConfig(RobotConfig):
             "image_left": OpenCVCameraConfig(
                 camera_index=3,
                 fps=30,
-                width=320,
-                height=240,
+                width=640,
+                height=480,
             ),
             # "image_depth_top": OpenCVCameraConfig(
             #     camera_index=4,
